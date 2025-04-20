99264ec98e6e595c9e053ef0ef5416d501894c4c
Aditya <Adityapandey18501@gmail.com>
2025-04-20 12:47:50 +0530

    consoling the error

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,6 +24,10 @@ app.use(cors({
 }
 ))
 
+app.use((req, res, next) => {
+    console.log("Incoming request:", req.method, req.url);
+    next();
+  });
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
