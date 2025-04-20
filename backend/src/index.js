de4cf22568a16faacf38f927b5f1ed7e4db36de4
Aditya <Adityapandey18501@gmail.com>
2025-04-20 13:37:38 +0530

    test 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,14 +32,23 @@ app.use((req, res, next) => {
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-    app.get('/*',(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+//     app.get('/*',(req,res)=>{
+//         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+//     })
+// }
+
+if (process.env.NODE_ENV === "production") {
+    const frontendPath = path.join(__dirname, "../frontend/dist");
+    
+    app.use(express.static(frontendPath));
+    
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(frontendPath, "index.html"));
+    });
 }
-
 
 
 server.listen(PORT,()=>{
