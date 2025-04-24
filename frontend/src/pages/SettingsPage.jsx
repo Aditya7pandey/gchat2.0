0b600338c2123c91c626d63cc4b5ae94a84cad90
Aditya <Adityapandey18501@gmail.com>
2025-04-24 20:38:59 +0530

    Updated UI features

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function SettingsPage() {
   return (
-    <div>SettingsPage</div>
+    
+    <div className='h-full w-full pt-40 flex justify-center items-center'>
+      <h1 className='text-3xl'>In Progress</h1>
+    </div>
+
   )
 }
 
